a78a441ee00b4549dcad0e9f4f470e0550cc5d06
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-02-28 14:53:04 +0100

    added dashboard js ans css

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./Dashboard.css";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <section>
+        <p>Welcome to The No Name App!</p>
+      </section>
+    );
+  }
+}
+
+export default Dashboard;
