5ea4ab260eb5832d0aef081b6a96248cadeb2a54
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-02-28 15:08:15 +0100

    set up login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,3 +1,66 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import "./LoginPage.css";
+import { postLogIn } from "../api";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      originalPassword: ""
+    };
+  }
+
+  genericOnChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    postLogIn(this.state).then(response => {
+      console.log("Log In", response.data);
+      this.props.loginSuccess(response.data);
+    });
+  }
+
+  render() {
+    return this.props.currentUser ? (
+      <Redirect to="/Dashboard" />
+    ) : (
+      <section className="LoginPage">
+        <h2>Log In</h2>
+
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <label>
+            Email:
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.email}
+              name="email"
+              type="email"
+              placeholder="john.doe@example.com"
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.originalPassword}
+              name="originalPassword"
+              type="password"
+              placeholder="secret secret"
+            />
+          </label>
+          <button>Log In</button>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default LoginPage;
