9d6adceaa265b14e77420665a50a6eb31ecdd821
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-02-28 14:04:02 +0100

    added contructor, navlink and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
+import Dashbord from "./components/Dashbord.js";
+import NotFound from "./components/NotFound.js";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import { getLogOut } from "./api";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    let userInfo = localStorage.getItem("currentUser");
+    if (userInfo) {
+      userInfo = JSON.parse(userInfo);
+    }
+
+    this.state = {
+      currentUser: userInfo
+    };
+  }
+
+  updateUser(newUser) {
+    if (newUser) {
+      localStorage.setItem("currentUser", JSON.stringify(newUser));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+    this.setState({ currentUser: newUser });
+  }
+
+  logoutClick() {
+    getLogOut().then(response => {
+      console.log("Log Out", response.data);
+      this.updateUser(null);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            {this.state.currentUser ? (
+              <span>
+                <NavLink exact to="/">
+                  Dashbord
+                </NavLink>
+                <NavLink to="/activity">Activity</NavLink>
+                <NavLink to="/charity">Charity</NavLink>
+                <NavLink to="/history">History</NavLink>
+                <NavLink to="/profile">Profile</NavLink>
+              </span>
+            ) : (
+              <span>
+                <NavLink to="/signup-page">Sign Up</NavLink>
+                <NavLink to="/login-page">Log In</NavLink>
+              </span>
+            )}
+          </nav>
         </header>
+
+        <Switch>
+          <Route exact path="/" component={Dashbord} />
+          <Route
+            path="/signup-page"
+            render={() => {
+              return (
+                <SignupPage
+                  currentUser={this.state.currentUser}
+                  signupSuccess={user => this.updateUser(user)}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/login-page"
+            render={() => {
+              return (
+                <LoginPage
+                  currentUser={this.state.currentUser}
+                  loginSuccess={user => this.updateUser(user)}
+                />
+              );
+            }}
+          />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
