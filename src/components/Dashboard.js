33be8a6acf97d14e991eab99e30e40e681cc9337
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-03-01 17:51:07 +0100

    setup dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,10 +3,40 @@ import React, { Component } from "react";
 import "./Dashboard.css";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      TimeSaved: "",
+      currentDate: "",
+      logo: ""
+    };
+  }
   render() {
     return (
       <section>
         <p>Welcome to TimeFor!</p>
+
+        <div>
+          <p>TimeSaved</p>
+          {/* <p>{this.state.TimeSaved}</p> */}
+        </div>
+        <div>
+          <p>date: february 2019</p>
+        </div>
+        <div>CHART</div>
+        <div>
+          <button>Add Time</button>
+          <button>Spend Time</button>
+        </div>
+        <div>
+          <h5>Recently Added</h5>
+        </div>
+        <div>
+          <ul>
+            <li>LOGO + ACTIVITY + TIME SAVED</li>
+          </ul>
+        </div>
       </section>
     );
   }
