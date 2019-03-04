032f5590b98dc1e260ce0386db671d71abf50010
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-03-04 16:54:00 +0100

    import render and gave width

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import { render } from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
