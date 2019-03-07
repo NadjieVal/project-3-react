a7d0680d4e3bfd591f0e6170791bab2cdc3227c7
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-03-07 10:54:49 +0100

    working on the mission checkout and accomplished

--- src/components/YourMissions.js
+++ src/components/YourMissions.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
 
 import "./YourMissions.css";
+import { getMissionHistory } from "../api";
 
 class YourMissions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      charityLogo: "",
+      missionName: "",
+      charityName: "",
+      missionAccomplished: []
+    };
+  }
+
+  componentDidMount() {
+    getMissionHistory().then(response => {
+      console.log("mission mission mission", response.data);
+      this.setState({ missionAccomplished: response.data });
+    });
+  }
   render() {
+    const { missionAccomplished } = this.state;
     return (
-      <div className="YourMissions">
-        <h3>The Missions you've accomplished!</h3>
-      </div>
+      <section>
+        <div className="YourMissions">
+          <h3>The Missions you've accomplished!</h3>
+          <div>
+            <div missionAccomplished={missionAccomplished}>
+              <div className="container">
+                <div className="row">
+                  <div className="col-8">
+                    <img src={this.state.charityLogo} alt="charityLogo" />
+                    <div className="col-4">
+                      <p>{this.state.charityName}</p>
+                      <p>{this.state.missionName}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
