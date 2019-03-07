5333f4fa01824a2860ff26bfc12b741e90f888cf
Poeli Bojorquez <pbojorquez@poeli.com>
2019-03-07 19:06:15 +0100

    corrected logout state

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Switch, Route, NavLink } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
-import Home from "../components/Home.js";
 import { getLogOut } from "../api.js";
 
 import "./Profile.css";
@@ -9,53 +8,29 @@ import "./Profile.css";
 class Profile extends Component {
   constructor(props) {
     super(props);
-    let userInfo = localStorage.getItem("project3User");
-    if (userInfo) {
-      userInfo = JSON.parse(userInfo);
-    }
 
     this.state = {
-      project3User: userInfo,
       submitted: false
     };
-  }
-
-  updateUser(newUser) {
-    if (newUser) {
-      localStorage.setItem("project3User", JSON.stringify(newUser));
-    } else {
-      localStorage.removeItem("project3User");
-    }
-    this.setState({ project3User: newUser });
   }
 
   logoutClick() {
     getLogOut().then(response => {
       console.log("Log Out", response.data);
       this.setState({ submitted: true });
-      this.updateUser(null);
+      this.props.logoutSuccess(null);
     });
   }
 
   render() {
     const { project3User } = this.props;
 
+    if (this.state.submitted) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <section className="Profile container">
-        <Switch>
-          <Route
-            path="/logout"
-            render={() => {
-              return (
-                <Home
-                  project3User={this.state.project3User}
-                  logoutSuccess={newUser => this.updateUser(newUser)}
-                />
-              );
-            }}
-          />
-        </Switch>
-
         <div className="marginBottom row justify-content-center marginSides">
           <div className="h-auto col-lg-6 col-md-8 col-sm-10 m-auto addMargins d-flex flex-column align-items-center">
             <h1>Profile</h1>
@@ -65,9 +40,9 @@ class Profile extends Component {
             </h3>
             <p className="line-height">{project3User.email}</p>
 
-            <NavLink to="/logout" onClick={() => this.logoutClick()}>
-              <button className="primary-btn">Log Out</button>
-            </NavLink>
+            <button className="primary-btn" onClick={() => this.logoutClick()}>
+              Log Out
+            </button>
           </div>
         </div>
       </section>
