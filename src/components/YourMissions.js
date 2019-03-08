1ca0a1480f47fe79f69c5c10a62aa8ece3e9f15a
Poeli Bojorquez <pbojorquez@poeli.com>
2019-03-08 13:26:08 +0100

    your-missions update

--- src/components/YourMissions.js
+++ src/components/YourMissions.js
@@ -24,35 +24,39 @@ class YourMissions extends Component {
     const { missionAccomplished } = this.state;
     return (
       <section div className="App container">
-        <div className="marginBottom row justify-content-center marginSides">
+        <div className="marginBottom row justify-content-center">
           {/* <div className="YourMissions"> */}
-          <h1>Accomplished Missions</h1>
-          <div>
-            {missionAccomplished.map(oneMission => {
-              return (
+          <h1 className="col-12">Accomplished Missions</h1>
+          {/* <div> */}
+          {missionAccomplished.map(oneMission => {
+            return (
+              <div
+                key={oneMission._id}
+                // className="col-12 d-flex history-card"
+                className="text-center col-lg-4 col-md-6 col-sm-12 justify-content-center"
+              >
+                {/* <div className=""> */}
                 <div
-                  key={oneMission._id}
-                  className="col-12 d-flex history-card"
+                  // className="col-6 padding-left"
+                  className="history-card d-flex flex-column align-items-center"
                 >
-                  {/* <div className=""> */}
-                  <div className="col-6 padding-left">
-                    <img
-                      src={oneMission.charityLogo}
-                      className="history-logos w-100 remove-padding"
-                      alt={oneMission.charityName}
-                    />
-                  </div>
-                  <div className="col-6 line-height remove-padding">
-                    <p className="bold-text line-height">
-                      {oneMission.missionName}
-                    </p>
-                    <p className="line-height">{oneMission.charityName}</p>
-                  </div>
-                  {/* </div>   */}
+                  <img
+                    src={oneMission.charityLogo}
+                    className="charity-logo"
+                    alt={oneMission.charityName}
+                  />
+                  {/* </div> */}
+                  {/* <div className="col-6 line-height remove-padding"> */}
+                  <p className="bold-text line-height">
+                    {oneMission.missionName}
+                  </p>
+                  <p className="line-height">{oneMission.charityName}</p>
                 </div>
-              );
-            })}
-          </div>
+                {/* </div>   */}
+              </div>
+            );
+          })}
+          {/* </div> */}
           {/* </div> */}
         </div>
       </section>
